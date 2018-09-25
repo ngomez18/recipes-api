7514668096a717f4d4fc8678217236a1b5ee74eb
Nicolás Gómez <ngomezgg.18@gmail.com>
2018-09-25 17:06:50 -0500

    Implement methods to get all Ingredients and a single ingredient

--- entities/ingredient.go
+++ entities/ingredient.go
@@ -7,12 +7,14 @@ import (
 
 // Ingredient model object
 type Ingredient struct {
+	ID   int    `json:"id"`
 	Name string `json:"name"`
 	Type string `json:"type"`
 }
 
 func (p *Ingredient) getIngredient(db *sql.DB) error {
-	return errors.New("Not implemented")
+	return db.QueryRow("SELECT * FROM ingredients WHERE id=$1",
+		p.ID).Scan(&p.Name, &p.Type)
 }
 
 func (p *Ingredient) updateIngredient(db *sql.DB) error {
@@ -28,5 +30,25 @@ func (p *Ingredient) createIngredient(db *sql.DB) error {
 }
 
 func getIngredients(db *sql.DB, start, count int) ([]Ingredient, error) {
-	return nil, errors.New("Not implemented")
+	rows, err := db.Query(
+		"SELECT id, name, type FROM ingredients LIMIT $1 OFFSET $2",
+		count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	ingredients := []Ingredient{}
+
+	for rows.Next() {
+		var i Ingredient
+		if err := rows.Scan(&i.ID, &i.Name, &i.Type); err != nil {
+			return nil, err
+		}
+		ingredients = append(ingredients, i)
+	}
+
+	return ingredients, nil
 }
